0749304ef4f502add7628fbd61d5119f275c49db
RashmiTimalsina1 <rashni.221738@ncit.edu.np>
2026-07-19 16:26:53 +0545

    Created admin navbar

---
+++ admin/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h2>Food Delivery Admin Panel</h2>
+
+      <div className="profile">
+        👤 Admin
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
