befec1e4f0f6d39ae8ef7306322a86398454ac5a
RashmiTimalsina1 <rashni.221738@ncit.edu.np>
2026-07-22 12:15:33 +0545

    Implemented food listing feature in admin panel

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,7 +1,36 @@
+import "./List.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { url } from "../../utils/config";
+
 const List = () => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+  try {
+    const response = await axios.get(`${url}/api/food/list`);
+
+    console.log("Response:", response.data);
+
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      alert("Error fetching food list");
+    }
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
   return (
-    <div>
-      <h2>List Items</h2>
+    <div className="list add">
+      <h2>All Food List</h2>
+
+      <p>Total Foods: {list.length}</p>
     </div>
   );
 };
