cfedd02f8b7ccc4d8560f5626150a3b6c3412500
RashmiTimalsina1 <rashni.221738@ncit.edu.np>
2026-07-23 10:58:48 +0545

    Integrated frontend authentication setup and toast notifications

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import App from "./App";
 import "./index.css";
 
-import StoreContextProvider from "./context/StoreContext";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StoreContextProvider>
+  <BrowserRouter>
+    <>
+      <ToastContainer />
       <App />
-  </StoreContextProvider>
+    </>
+  </BrowserRouter>
 );
