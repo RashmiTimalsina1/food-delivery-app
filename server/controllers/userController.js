2c69592ecb1d3f59e1351870be0f3bc843507655
RashmiTimalsina1 <rashni.221738@ncit.edu.np>
2026-07-31 16:55:00 +0545

    Implemented user registration API

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -69,7 +69,66 @@ const registerUser = async (req, res) => {
     });
   }
 };
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
+    // Check if user already exists
+    const exists = await userModel.findOne({ email });
+
+    if (exists) {
+      return res.json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    // Validate email
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+
+    // Validate password
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Password should be at least 8 characters",
+      });
+    }
+
+    // Encrypt password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create new user
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    // Generate token
+    const token = createToken(user._id);
+
+    res.json({
+      success: true,
+      token,
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    res.json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
 // Login User
 const loginUser = async (req, res) => {
   try {
